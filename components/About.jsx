a8c9db88ffb635a9ba07230e17ef6f98054d956b
CarlosJunioor <carlosjunior866@gmail.com>
2022-11-05 05:23:50 +0000

    fix: about

--- components/About.jsx
+++ components/About.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
-import AboutImg from '../public/assets/about.png';
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import AboutImg from "../public/assets/about.png";
 
 const About = () => {
   return (
-    <div id='about' className='w-full md:h-screen p-2 flex items-center py-16'>
-      <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
-        <div className='col-span-2'>
-          <p className='uppercase text-xl tracking-widest text-[#5651e5]'>
+    <div id="about" className="w-full md:h-screen p-2 flex items-center py-16">
+      <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
+        <div className="col-span-2">
+          <p className="uppercase text-xl tracking-widest text-[#5651e5]">
             About
           </p>
-          <h2 className='py-4'>Who I Am</h2>
-          <p className='py-2 text-gray-600'>
+          <h2 className="py-4">Who I Am</h2>
+          <p className="py-2 text-gray-600">
             I specialize in building mobile responsive front-end UI applications
-            that connect with API’s and other backend technologies. I’m
+            that connect with APIs and other backend technologies. Im
             passionate about learning new technologies and understand there is
             more than one way to accomplish a task. Though I am most proficient
             in building front-end applications using HTML, CSS, Javascript, and
@@ -22,20 +22,23 @@ const About = () => {
             needed. I believe that being a great developer is not using one
             specific language, but choosing the best tool for the job.
           </p>
-          <p className='py-2 text-gray-600'>
-          Im currently doing a career shift from Salesperson to Frontend Development!
-I've been learning Web Development for 6 months and it has really grown on me! It has become my passion to develop cool-looking websites that people love to use.
-SouJunior is Open-Source project that have 16.000 followers on LinkedIn and with 30 people at least working on the project.
-Passionate about Web3 and NFT's
+          <p className="py-2 text-gray-600">
+            Im currently doing a career shift from Salesperson to Frontend
+            Development! I've been learning Web Development for 6 months and it
+            has really grown on me! It has become my passion to develop
+            cool-looking websites that people love to use. SouJunior is
+            Open-Source project that have 16.000 followers on LinkedIn and with
+            30 people at least working on the project. Passionate about Web3 and
+            NFT's
           </p>
-          <Link href='/#projects'>
-            <p className='py-2 text-gray-600 underline cursor-pointer'>
+          <Link href="/#projects">
+            <p className="py-2 text-gray-600 underline cursor-pointer">
               Check out some of my latest projects.
             </p>
           </Link>
         </div>
-        <div className='w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
-          <Image src={AboutImg} className='rounded-xl' alt='/' />
+        <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
+          <Image src={AboutImg} className="rounded-xl" alt="/" />
         </div>
       </div>
     </div>
